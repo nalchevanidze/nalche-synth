aa6d1e4d53e15f812dccc390979f24a2be2c881a
daviti nalchevanidze <d.nalchevanidze@gmail.com>
2017-09-28 18:25:58 +0200

    update

---
+++ src/midiPanel/MidiHeader.js
@@ -0,0 +1,54 @@
+import React from "react";
+import HeaderButton from "./HeaderButton";
+export default class MidiHeader extends React.Component {
+	render() {
+		let { stop, setBPM, BPM, isPlayng } = this.props.global;
+		let id = isPlayng ? "pause" : "play";
+		return (
+			<section
+				style={{
+					background: "#444",
+					color: "#ffa929",
+					padding: "5px",
+					border: "1px solid #333",
+					display: "flex"
+				}}
+			>
+				<HeaderButton id={id} actions={this.props.global} />
+				<HeaderButton id={"stop"} actions={this.props.global} />
+
+
+				<div
+					style={{
+						padding: "5px",
+						fontSize: "12px",
+					}}
+				>
+					<label>BPM</label>
+					<input
+						style={{
+							background: "#444",
+							border: "none",
+							color: "#ffa929",
+							borderBottom: "1px solid"
+						}}
+						className="bpm-value"
+						onChange={setBPM}
+						defaultValue={BPM()}
+
+					/>
+				</div>
+				<HeaderButton
+					id={"draw"}
+					color={this.props.actionType === "draw" ? "#ffa929" : "#777"}
+					actions={this.props.setMode}
+				/>
+				<HeaderButton
+					id={"select"}
+					color={this.props.actionType === "select" ? "#ffa929" : "#777"}
+					actions={this.props.setMode}
+				/>
+			</section>
+		)
+	}
+}
--- src/midiPanel/index.js
+++ src/midiPanel/index.js
@@ -1,60 +1,8 @@
 import React from "react";
 import KeyboardSVG from "./KeyboardSVG";
-import HeaderButton from "./HeaderButton";
+import MidiHeader from "./MidiHeader";
 
 
-class Header extends React.Component {
-	render() {
-		let { stop, setBPM, BPM, isPlayng } = this.props.global;
-		let id = isPlayng ? "pause" : "play";
-		return (
-			<section
-				style={{
-					background: "#444",
-					color: "#ffa929",
-					padding: "5px",
-					border: "1px solid #333",
-					display: "flex"
-				}}
-			>
-				<HeaderButton id={id} actions={this.props.global} />
-				<HeaderButton id={"stop"} actions={this.props.global} />
-
-
-				<div
-					style={{
-						padding: "5px",
-						fontSize: "12px",
-					}}
-				>
-					<label>BPM</label>
-					<input
-						style={{
-							background: "#444",
-							border: "none",
-							color: "#ffa929",
-							borderBottom: "1px solid"
-						}}
-						className="bpm-value"
-						onChange={setBPM}
-						defaultValue={BPM()}
-
-					/>
-				</div>
-				<HeaderButton
-					id={"draw"}
-					color={this.props.actionType === "draw" ? "#ffa929" : "#777"}
-					actions={this.props.setMode}
-				/>
-				<HeaderButton
-					id={"select"}
-					color={this.props.actionType === "select" ? "#ffa929" : "#777"}
-					actions={this.props.setMode}
-				/>
-			</section>
-		)
-	}
-}
 class MidiDesk extends React.PureComponent {
 	render() {
 		let { midi, updateMidi, global, currentState = 0 } = this.props;
@@ -102,7 +50,7 @@ export default class melody extends React.PureComponent {
 					position: "relative",
 				}}
 			>
-				<Header
+				<MidiHeader
 					global={global}
 					setMode={this.modes}
 					actionType={this.state.actionType}
