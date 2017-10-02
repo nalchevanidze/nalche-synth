a917c751386eb3ebce81d07ded3d5d6e6a846516
daviti nalchevanidze <d.nalchevanidze@gmail.com>
2017-10-02 23:12:18 +0200

    filterUpdate

--- src/oscillator/index.js
+++ src/oscillator/index.js
@@ -4,36 +4,30 @@ const { destination } = Context;
 import NoteToFrequency from "./NoteToFrequency";
 import SoundEvent from "./SoundEvent";
 const bufferSize = 2048; //4096;
-//import MoogFilter from "./MoogFilter";
 import filterBuilder from "./filterBuilder";
 
 export default function Oscillator() {
 
 	const event = SoundEvent();
-	//const filter = MoogFilter();
-	const sampler = filterBuilder();
+	const filter = filterBuilder();
 
 	function onProcess({ outputBuffer }) {
 		let audio = outputBuffer.getChannelData(0);
 		if (event.eventTimes.live) {
-			FillAudioChenel(audio, event, sampler);
+			FillAudioChenel(audio, event, filter);
 		} else {
 			audio.fill(0);
 		}
 	}
 	const node = Context.createScriptProcessor(bufferSize, 1, 1);
-	//	node.connect(filter);
-	//	filter.connect(destination);
 	node.connect(destination);
 	node.onaudioprocess = onProcess;
-
 
 	return {
 		start(param) {
 			let frequency = NoteToFrequency(param.note);
 			event.reset(frequency);
-			sampler.set();
-			//filter.start();
+			filter.set();
 		},
 		end() {
 			event.end();
