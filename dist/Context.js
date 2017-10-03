250e4b5486c1a7842c1f02eb6a9caf5029fc4d19
daviti nalchevanidze <d.nalchevanidze@gmail.com>
2017-10-04 00:38:19 +0200

    update

--- dist/Context.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var Context = new (window.AudioContext || window.webkitAudioContext)();
-exports.default = Context;
