333c5e80ab18161c433f118bcf5eeb6b455006fd
daviti nalchevanidze <d.nalchevanidze@gmail.com>
2017-10-05 00:41:38 +0200

    asf

--- dist/audio/oscillator/SafeWaveValue.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-exports.default = SafeWaveValue;
-function SafeWaveValue(value) {
-	return Math.min(Math.max(Number.isNaN(value) ? 0 : value, -1), 1);
-}
--- dist/keymap.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-exports.default = ["z", "s", "x", "d", "c", "v", "g", "b", "h", "n", "j", "m", "q", "2", "w", "3", "e", "r", "5", "t", "6", "y", "7", "u"];
