b2598ee053f981fb75299a9d94df7ccf7ebb7654
nalchevanidze <d.nalchevanidze@gmail.com>
2017-12-04 00:24:46 +0100

    update

--- dist/audio/oscillator/MoogSampler.js
+++ dist/audio/oscillator/MoogSampler.js
@@ -4,37 +4,50 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 exports.default = MoogSampler;
+/* 
+acourding:
+	http://www.musicdsp.org/showArchiveComment.php?ArchiveID=26
+	https://noisehack.com/custom-audio-effects-javascript-web-audio-api/
+*/
+
 function MoogSampler() {
-	var in1 = void 0,
-	    in2 = void 0,
-	    in3 = void 0,
-	    in4 = void 0,
-	    out1 = void 0,
-	    out2 = void 0,
-	    out3 = void 0,
-	    out4 = void 0;
-	in1 = in2 = in3 = in4 = out1 = out2 = out3 = out4 = 0;
+
+	var in1 = 0;
+	var in2 = 0;
+	var in3 = 0;
+	var in4 = 0;
+	var out1 = 0;
+	var out2 = 0;
+	var out3 = 0;
+	var out4 = 0;
 	var fb = 0;
+	var diff = 0;
+
+	var pole = function pole(input, output) {
+		return 0.3 * input + diff * output;
+	};
 
 	return function sampleGenerate(inputSample, f, resonance) {
 
-		fb = resonance * 4 * (1.0 - 0.15 * f * f);
+		var fpower = Math.pow(f, 2);
+		diff = 1 - f;
 
+		fb = resonance * 4 * (1.0 - 0.15 * fpower);
 		inputSample -= out4 * fb;
+		inputSample *= 0.35013 * Math.pow(fpower, 2);
 
-		inputSample *= 0.35013 * Math.pow(f, 4);
-		out1 = inputSample + 0.3 * in1 + (1 - f) * out1; // Pole 1
-
+		out1 = inputSample + pole(in1, out1); // Pole 1
 		in1 = inputSample;
-		out2 = out1 + 0.3 * in2 + (1 - f) * out2; // Pole 2
 
+		out2 = out1 + pole(in2, out2); // Pole 2
 		in2 = out1;
-		out3 = out2 + 0.3 * in3 + (1 - f) * out3; // Pole 3
 
+		out3 = out2 + pole(in3, out3); // Pole 3
 		in3 = out2;
-		out4 = out3 + 0.3 * in4 + (1 - f) * out4; // Pole 4
 
+		out4 = out3 + pole(in4, out4); // Pole 4
 		in4 = out3;
+
 		return out4;
 	};
 }
--- src/audio/oscillator/MoogSampler.js
+++ src/audio/oscillator/MoogSampler.js
@@ -5,29 +5,45 @@ acourding:
 */
 
 export default function MoogSampler() {
-	let in1, in2, in3, in4, out1, out2, out3, out4;
-	in1 = in2 = in3 = in4 = out1 = out2 = out3 = out4 = 0;
+
+	let in1 = 0;
+	let in2 = 0;
+	let in3 = 0;
+	let in4 = 0;
+	let out1 = 0;
+	let out2 = 0;
+	let out3 = 0;
+	let out4 = 0;
 	let fb = 0;
+	let diff = 0;
+
+	const pole = (input, output) => (
+		0.3 * input +
+		diff * output
+	);
+
 
 	return function sampleGenerate(inputSample, f, resonance) {
 
-		fb = (resonance * 4) * (1.0 - 0.15 * f * f);
-
+		let fpower = f ** 2;
+		diff = 1 - f;
+		
+		fb = (resonance * 4) * (1.0 - 0.15 * fpower);
 		inputSample -= out4 * fb;
+		inputSample *= 0.35013 * (fpower ** 2);
 
-		inputSample *= 0.35013 * (f ** 4);
-		out1 = inputSample + 0.3 * in1 + (1 - f) * out1; // Pole 1
-
+		out1 = inputSample + pole(in1, out1); // Pole 1
 		in1 = inputSample;
-		out2 = out1 + 0.3 * in2 + (1 - f) * out2; // Pole 2
 
+		out2 = out1 + pole(in2, out2); // Pole 2
 		in2 = out1;
-		out3 = out2 + 0.3 * in3 + (1 - f) * out3; // Pole 3
 
+		out3 = out2 + pole(in3, out3); // Pole 3
 		in3 = out2;
-		out4 = out3 + 0.3 * in4 + (1 - f) * out4; // Pole 4
 
+		out4 = out3 + pole(in4, out4); // Pole 4
 		in4 = out3;
+
 		return out4;
 	};
 
