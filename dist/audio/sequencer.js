6c0e7e3ab5a28b278e31fade77d81ed2d659e650
nalchevanidze <d.nalchevanidze@gmail.com>
2017-12-08 23:35:22 +0100

    update

--- dist/audio/sequencer.js
+++ dist/audio/sequencer.js
@@ -1,94 +1,73 @@
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var sequence = [[1, 2, 3], [], [], [1, 2, 3], [], [], [1, 2, 3], [], [1, 2, 3], [], [1], [2], [3], [2], [], []];
-var arpIndex = 0;
-
+Object.defineProperty(exports, "__esModule", { value: true });
+let sequence = [
+    [1, 2, 3],
+    [],
+    [],
+    [1, 2, 3],
+    [],
+    [],
+    [1, 2, 3],
+    [],
+    [1, 2, 3],
+    [],
+    [1],
+    [2],
+    [3],
+    [2],
+    [],
+    []
+];
+let arpIndex = 0;
 function sequencer() {
-	arpIndex = 0;
-	var endIndex = sequence.length;
-	return {
-		next: function next(notes) {
-			var chord = [];
-			var maxNotes = notes.size;
-
-			var values = Array.from(notes).sort(function (a, b) {
-				return a > b ? 1 : -1;
-			});
-
-			if (maxNotes) {
-				chord = sequence[arpIndex].map(function (i) {
-					i--;
-					var octave = 12 * Math.floor(i / maxNotes);
-					var selectIndex = i % maxNotes;
-					return values[selectIndex] + octave;
-				});
-			}
-			arpIndex++;
-
-			if (arpIndex >= endIndex) {
-				arpIndex = 0;
-			}
-
-			return chord;
-		}
-	};
+    arpIndex = 0;
+    let endIndex = sequence.length;
+    return {
+        next(notes) {
+            let chord = [];
+            let maxNotes = notes.size;
+            let values = Array.from(notes).sort((a, b) => a > b ? 1 : -1);
+            if (maxNotes) {
+                chord = sequence[arpIndex].map((i) => {
+                    i--;
+                    const octave = 12 * Math.floor(i / maxNotes);
+                    const selectIndex = i % maxNotes;
+                    return values[selectIndex] + octave;
+                });
+            }
+            arpIndex++;
+            if (arpIndex >= endIndex) {
+                arpIndex = 0;
+            }
+            return chord;
+        }
+    };
 }
-
-var oldChord = [];
-var seq = sequencer();
-var steps = 2;
-
-var Sequencer = function () {
-	function Sequencer() {
-		_classCallCheck(this, Sequencer);
-
-		this.state = 0;
-		this.setSequence = this.setSequence.bind(this);
-		this.restart = this.restart.bind(this);
-	}
-
-	_createClass(Sequencer, [{
-		key: "next",
-		value: function next(main) {
-			if (this.state >= steps) {
-
-				var chord = seq.next(main.active);
-
-				oldChord.forEach(function (v) {
-					return main.simpleUnset(v);
-				});
-
-				chord.forEach(function (v) {
-					return main.simpleSet(v);
-				});
-
-				oldChord = chord;
-				this.state = 0;
-			}
-			this.state++;
-		}
-	}, {
-		key: "setSequence",
-		value: function setSequence(seq) {
-			this.sequence = seq;
-			sequence = seq;
-		}
-	}, {
-		key: "restart",
-		value: function restart() {
-			arpIndex = 0;
-		}
-	}]);
-
-	return Sequencer;
-}();
-
+let oldChord = [];
+const seq = sequencer();
+let steps = 2;
+class Sequencer {
+    constructor() {
+        this.state = 0;
+        this.setSequence = this.setSequence.bind(this);
+        this.restart = this.restart.bind(this);
+    }
+    next(main) {
+        if (this.state >= steps) {
+            let chord = seq.next(main.active);
+            oldChord.forEach(v => main.simpleUnset(v));
+            chord.forEach(v => main.simpleSet(v));
+            oldChord = chord;
+            this.state = 0;
+        }
+        this.state++;
+    }
+    setSequence(seq) {
+        this.sequence = seq;
+        sequence = seq;
+    }
+    restart() {
+        arpIndex = 0;
+    }
+}
 exports.default = Sequencer;
--- src/audio/sequencer.ts
+++ src/audio/sequencer.ts
@@ -20,11 +20,11 @@ let arpIndex: number = 0;
 
 function sequencer() {
 	arpIndex = 0;
-	let endIndex:number = sequence.length;
+	let endIndex: number = sequence.length;
 	return {
-		next(notes) {
-			let chord = [];
-			let maxNotes = notes.size;
+		next(notes: Set<number>) {
+			let chord: number[] = [];
+			let maxNotes: number = notes.size;
 
 			let values = Array.from(notes).sort(
 				(a, b) => a > b ? 1 : -1
@@ -33,7 +33,7 @@ function sequencer() {
 			if (maxNotes) {
 				chord = sequence[arpIndex].map((i) => {
 					i--;
-					const octave = 12 * Math.floor(i / maxNotes);
+					const octave: number = 12 * Math.floor(i / maxNotes);
 					const selectIndex = i % maxNotes;
 					return values[selectIndex] + octave;
 				});
@@ -51,9 +51,12 @@ function sequencer() {
 
 let oldChord = [];
 const seq = sequencer();
-let steps:number = 2;
+let steps: number = 2;
 
 export default class Sequencer {
+
+	state: number;
+	sequence: number[][];
 
 	constructor() {
 		this.state = 0;
@@ -61,30 +64,27 @@ export default class Sequencer {
 		this.restart = this.restart.bind(this);
 	}
 
-	next(main) {
+	next(main): void {
 		if (this.state >= steps) {
-
 			let chord = seq.next(main.active);
-
 			oldChord.forEach(
 				v => main.simpleUnset(v)
 			);
-
 			chord.forEach(
 				v => main.simpleSet(v)
 			);
-
 			oldChord = chord;
 			this.state = 0;
 		}
 		this.state++;
 	}
-	setSequence(seq) {
+
+	setSequence(seq): void {
 		this.sequence = seq;
 		sequence = seq;
 	}
 
-	restart() {
+	restart(): void {
 		arpIndex = 0;
 	}
 }
